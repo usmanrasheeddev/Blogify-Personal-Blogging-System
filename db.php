84f22ee3ca14d55be6e4123c5a311ea93235d4e9
Usman Rasheed <usmanrasheed.dev@gmail.com>
2026-02-05 18:46:25 +0000

    Update DB config and ignore uploads

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "simple_blog");
+$conn = mysqli_connect("sql200.infinityfree.com", "if0_41084179", "00hm647j", "if0_41084179_simple_blog");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
